929acb886244a1cb0559aa323b34a87bbcbd4e02
Kenneth Carter <kccarter32@gmail.com>
2020-08-13 09:35:38 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# SubSonic.Core.Proxies
+this package implements proxy generation
+- Poco database proxies
+- RPC Proxies for remotely calling methods on remote objects
---
+++ SubSonic.Core.Proxies/ProxyBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace SubSonic.Core.Proxies
+{
+    public sealed class ProxyBuilder
+    {
+        public string ProxyName { get; set; }
+        public Type InterfaceType { get; set; }
+        public Type CtorType { get; set; }
+        public AssemblyBuilder AssemblyBuilder { get; set; }
+        public ModuleBuilder ModuleBuilder { get; set; }
+        public TypeBuilder TypeBuilder { get; set; }
+    }
+}
